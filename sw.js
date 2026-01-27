36c36b58563107e63bc28890b08a89e649a40e53
utajeny <133534756+utajeny@users.noreply.github.com>
2026-01-28 00:28:37 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,22 @@
-const CACHE = "fitlog-v1";
+const CACHE = "fitlog-v2";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.addAll(["./", "./index.html"])
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
     )
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
   );
 });
